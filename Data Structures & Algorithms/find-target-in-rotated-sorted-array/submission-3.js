a77dcad9b62fe473421e18a6737cb768d22f3047
Eyobel11 <154692239+Eyobel11@users.noreply.github.com>
2026-09-14 21:11:50 -0400

    Add: find-target-in-rotated-sorted-array - submission-3

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-3.js
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+        let left = 0
+        let right = nums.length - 1
+
+        while(left <= right){
+
+           let middle = Math.floor((left + right)/2)
+            if(nums[middle] == target){
+                return middle;
+            }
+            else if(nums[left] <= nums[middle]){
+                if( nums[left] <=target && target <= nums[middle]){ 
+                    right = middle -1
+                }else{
+                    left = middle + 1
+                }
+
+            }else if(nums[left] >= nums[middle]){
+                if(nums[middle] <= target && target <= nums[right]){ 
+                    left = middle + 1
+                }else{
+                    right = middle -1
+                }
+
+            }
+        }
+        return -1
+    }
+}
